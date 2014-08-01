7b89bca965d238707fbe13d3547c054ffe820af2
zero13cool <zero13cool@yandex.ru>
2014-08-01 23:29:46 +0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+# coding: utf-8
+from setuptools import setup
+
+setup(
+    name='django-light-draft',
+    version='0.1',
+    author='Vladimir Savin',
+    author_email='zero13cool@yandex.ru',
+
+    description='Preview changes without saving the data into db.',
+    url='https://github.com/zerc/django-light-draft',
+    license='MIT',
+
+    packages=['light_draft'],
+    install_requires=[
+        'django>=1.6',
+        'South==1.0',
+    ],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+
+        'Framework :: Django',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
