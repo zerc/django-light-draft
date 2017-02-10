a67ad6766d311cfb210523ce7c8ed4f621733e58
Joe Jasinski <joe.jasinski@gmail.com>
2017-02-10 16:45:25 -0600

    updates to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-light-draft',
-    version='0.2a',
+    version='0.3.1',
     author='Vladimir Savin',
     author_email='zero13cool@yandex.ru',
 
