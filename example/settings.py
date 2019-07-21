19895f4cd34731589f68d5856736c560d9f8a86b
zero13cool <zero13cool@yandex.ru>
2019-07-21 16:44:59 +0100

    Fixed settings for Django22

--- example/settings.py
+++ example/settings.py
@@ -97,6 +97,7 @@ TEMPLATES = [
             'context_processors': [
                 'django.contrib.auth.context_processors.auth',
                 'django.template.context_processors.request',
+                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
